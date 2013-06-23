cb534cb69ac1de91ee04d84494bb1dfc673be767
Simon Arvidsson <simon.arvidsson@appland.se>
2013-06-23 16:38:16 +0200

    Made activity persistent for activty changes

--- greed/src/se/huvuddator/greed/MainActivity.java
+++ greed/src/se/huvuddator/greed/MainActivity.java
@@ -13,6 +13,30 @@ import android.widget.ToggleButton;
 public class MainActivity extends Activity {
 	private Random mRandom = new Random(System.currentTimeMillis());
 
+	// Static game constants
+	private static final int WIN_THRESHOLD = 10000;
+	private static final int MINIMUN_VALUE_TO_CONTINUE = 1;
+	public static final int NUMBER_OF_DICE = 6;
+
+	// Game state
+	private int[] mDiceValues = new int[NUMBER_OF_DICE];
+	private boolean[] mDiceChecked = new boolean[NUMBER_OF_DICE];
+	private boolean[] mDiceEnabled = new boolean[NUMBER_OF_DICE];
+	private int mScore = 0;
+	private int mTurnScore = 0;
+	private int mNumberOfTurns = 0;
+	private boolean mIsNewTurn = true;
+	private boolean mIsThrowEnabled = false;
+	private boolean mIsSavedEnabled = false;
+
+	// Views
+	private View mButtonThrow;
+	private TextView mTextScore;
+	private TextView mTextTurnScore;
+	private TextView mTextTurnCounter;
+	private TextView mTextLog;
+	private ToggleButton[] mDices = new ToggleButton[NUMBER_OF_DICE];
+	private View mButtonSave;
 	private static final int[] DiceDrawables = {
 		R.drawable.dice_1, 
 		R.drawable.dice_2,
@@ -22,30 +46,6 @@ public class MainActivity extends Activity {
 		R.drawable.dice_6,
 	};
 
-	private ToggleButton[] mDices = new ToggleButton[NUMBER_OF_DICE];
-
-	private int[] mDiceValues = new int[NUMBER_OF_DICE];
-	
-	public static final int NUMBER_OF_DICE = 6;
-	
-	private int mScore = 0;
-	private int mTurnScore = 0;
-	private static final int WIN_THRESHOLD = 10000;
-
-	private static final int MINIMUN_VALUE_TO_CONTINUE = 1;
-	private int mNumberOfTurns = 0;
-
-	private View mButtonThrow;
-	private TextView mTextScore;
-	private TextView mTextTurnScore;
-	private TextView mTextTurnCounter;
-	private TextView mTextLog;
-	
-	private View mButtonSave;
-
-	private int mLastThrowScore;
-
-	private boolean isNewTurn = true;
 
 
 	@Override
@@ -74,10 +74,63 @@ public class MainActivity extends Activity {
 	}
 
 
+	/*
+	 * 	private int[] mDiceValues = new int[NUMBER_OF_DICE];
+	private int mScore = 0;
+	private int mTurnScore = 0;
+	private int mNumberOfTurns = 0;
+	private boolean isNewTurn = true;
+	 */
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putIntArray("diceValues", mDiceValues);
+		outState.putBooleanArray("diceChecked", mDiceChecked);
+		outState.putBooleanArray("diceEnabled", mDiceEnabled);
+		outState.putInt("score", mScore);
+		outState.putInt("turnScore", mTurnScore);
+		outState.putInt("numberOfTurns", mNumberOfTurns);
+		outState.putBoolean("isNewTurn", mIsNewTurn);
+		outState.putBoolean("isThrowEnabled", mIsThrowEnabled);
+		outState.putBoolean("isSaveEnabled", mIsSavedEnabled);
+
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		mDiceValues = savedInstanceState.getIntArray("diceValues");
+		mDiceChecked = savedInstanceState.getBooleanArray("diceChecked");
+		mDiceEnabled = savedInstanceState.getBooleanArray("diceEnabled");
+		mScore = savedInstanceState.getInt("score");
+		mTurnScore = savedInstanceState.getInt("turnScore");
+		mNumberOfTurns = savedInstanceState.getInt("numberOfTurns");
+		mIsNewTurn = savedInstanceState.getBoolean("isNewTurn");
+		mIsThrowEnabled = savedInstanceState.getBoolean("isThrowEnabled");
+		mIsSavedEnabled = savedInstanceState.getBoolean("isSaveEnabled");
+		renderViews();
+	}
+
+
+	private void renderViews() {
+		mButtonThrow.setEnabled(mIsThrowEnabled);
+		mButtonSave.setEnabled(mIsSavedEnabled);
+
+		mTextScore.setText("Score: " +mScore);
+		mTextTurnScore.setText("Turn score: " +mTurnScore);
+		mTextTurnCounter.setText("Turns: " +mNumberOfTurns);
+
+		for (int i = 0; i < NUMBER_OF_DICE; i++) {
+			mDices[i].setBackgroundResource(DiceDrawables[mDiceValues[i]]);
+			mDices[i].setChecked(mDiceChecked[i]);
+			mDices[i].setEnabled(mDiceEnabled[i]);
+		}
+	}
+
 	public void onSave(View view) {
 		int[] scoringDiceValues = new int[mDiceValues.length];
-		for (int i = 0; i < mDices.length; i++) {
-			if(mDices[i].isEnabled()) {
+		for (int i = 0; i < NUMBER_OF_DICE; i++) {
+			if(mDiceEnabled[i]) {
 				scoringDiceValues[i] = mDiceValues[i];
 			} else {
 				scoringDiceValues[i] = -1;
@@ -87,31 +140,63 @@ public class MainActivity extends Activity {
 		mTurnScore += scoreResult.getScore();
 
 		boolean[] isDiceScoring = scoreResult.getIsDiceScoring();
-		for (int i = 0; i < isDiceScoring.length; i++) {
+		for (int i = 0; i < NUMBER_OF_DICE; i++) {
 			if(isDiceScoring[i]) {
-				mDices[i].setEnabled(false);
+				mDiceEnabled[i] = false;
 			}
 		}
-		
+
 		onEndTurn();
+		renderViews();
 	}
 
 	public void onClickDie(View view) {
-		int[] scoringDiceValues = new int[mDiceValues.length];
-		for (int i = 0; i < mDices.length; i++) {
-			if(mDices[i].isChecked()) {
+		// Should not be here if state is "new turn". Clean up and return.
+		if(mIsNewTurn) {
+			if (view instanceof ToggleButton) {
+				ToggleButton die = (ToggleButton) view;
+				die.setChecked(false);
+			}
+			mIsThrowEnabled = true;
+			return;
+		}
+
+		// Update game state
+		for (int i = 0; i < NUMBER_OF_DICE; i++) {
+			mDiceChecked[i] = mDices[i].isChecked();
+		}
+
+		int[] scoringDiceValues = new int[NUMBER_OF_DICE];
+		for (int i = 0; i < NUMBER_OF_DICE; i++) {
+			if(mDiceChecked[i]) {
 				scoringDiceValues[i] = mDiceValues[i];
 			} else {
 				scoringDiceValues[i] = -1;
 			}
 		}
-		
-		int checkedDiceScore = new ScoreResult(scoringDiceValues, mTextLog).getScore();
-		if(0 < checkedDiceScore) {
-			mButtonThrow.setEnabled(true);
-		} else {
-			mButtonThrow.setEnabled(false);
+
+		int[] possiblyScoringDice = new int[NUMBER_OF_DICE];
+		for (int i = 0; i < NUMBER_OF_DICE; i++) {
+			if(mDiceEnabled[i]) {
+				possiblyScoringDice[i] = mDiceValues[i];
+			} else {
+				possiblyScoringDice[i] = -1;
+			}
 		}
+
+		ScoreResult scoreResult = new ScoreResult(possiblyScoringDice, mTextLog);
+
+		boolean[] isDiceScoring = scoreResult.getIsDiceScoring();
+		for (int i = 0; i < NUMBER_OF_DICE; i++) {
+			if(mDiceChecked[i] && !isDiceScoring[i]) {
+				mDiceChecked[i] = false;
+			}
+		}
+
+		int checkedDiceScore = scoreResult.getScore();
+		mIsThrowEnabled = (0 < checkedDiceScore);
+
+		renderViews();
 	}
 
 
@@ -122,19 +207,17 @@ public class MainActivity extends Activity {
 	 */
 	public void onThrow(View view) {
 		// If this is a new turn, reset dice, otherwise end last throw.
-		if(isNewTurn) {
+		if(mIsNewTurn) {
 			setupTurn();
 		} else {
 			onEndThrow();
 		}
-		
-		mLastThrowScore = 0;
-		int[] scoringDiceValues = new int[mDiceValues.length];
-		
-		for (int i = 0; i < mDices.length; i++) {
-			if(mDices[i].isEnabled()) {
+
+		int[] scoringDiceValues = new int[NUMBER_OF_DICE];
+
+		for (int i = 0; i < NUMBER_OF_DICE; i++) {
+			if(mDiceEnabled[i]) {
 				int value = mRandom.nextInt(6);
-				mDices[i].setBackgroundResource(DiceDrawables[value]);
 				mDiceValues[i] = value;
 				scoringDiceValues[i] = mDiceValues[i];
 			} else {
@@ -142,44 +225,48 @@ public class MainActivity extends Activity {
 			}
 		}
 
-		mLastThrowScore = new ScoreResult(scoringDiceValues, mTextLog).getScore();
-		
-		if(0 == mLastThrowScore || (MINIMUN_VALUE_TO_CONTINUE > mLastThrowScore && 0 == mTurnScore)) {
+		int throwScore = new ScoreResult(scoringDiceValues, mTextLog).getScore();
+
+		if(0 == throwScore || (MINIMUN_VALUE_TO_CONTINUE > throwScore && 0 == mTurnScore)) {
 			mTurnScore = 0;
 			mTextLog.append("\nTurn ends with no points!");
+			Toast.makeText(this, "Turn ends with no points!", Toast.LENGTH_SHORT).show();
 			onEndTurn();
 		} else {
-			mButtonThrow.setEnabled(false);
-			mButtonSave.setEnabled(true);
+			mIsThrowEnabled = false;
+			mIsSavedEnabled = true;
 		}
+
+		renderViews();
 	}
 
 	private void setupTurn() {
 		for (int i = 0; i < mDices.length; i++) {
-			mDices[i].setChecked(false);
-			mDices[i].setEnabled(true);
+			mDiceChecked[i] = false;
+			mDiceEnabled[i] = true;
 		}
-		isNewTurn = false;
+		mIsNewTurn = false;
 	}
 
 	private void onEndThrow() {
-		int[] scoringDiceValues = new int[mDiceValues.length];
-		for (int i = 0; i < mDices.length; i++) {
-			if(mDices[i].isChecked()) {
+		int[] scoringDiceValues = new int[NUMBER_OF_DICE];
+		for (int i = 0; i < NUMBER_OF_DICE; i++) {
+			if(mDiceChecked[i]) {
 				scoringDiceValues[i] = mDiceValues[i];
-				mDices[i].setChecked(false);
-				mDices[i].setEnabled(false);
+				mDiceChecked[i] = false;
+				mDiceEnabled[i] = false;
 			} else {
 				scoringDiceValues[i] = -1;
 			}
 		}
 		mTurnScore += new ScoreResult(scoringDiceValues, mTextLog).getScore();
 		mTextTurnScore.setText("Turn score: " +mTurnScore);
-		
+
 		boolean hasScoredWithAll = new ScoreResult(mDiceValues, mTextLog).hasScoredWithAll();
 		if(hasScoredWithAll) {
-			for (int i = 0; i < mDices.length; i++) {
-				mDices[i].setEnabled(true);
+			Toast.makeText(this, "Scored with all!", Toast.LENGTH_SHORT).show();
+			for (int i = 0; i < NUMBER_OF_DICE; i++) {
+				mDiceEnabled[i] = true;
 			}
 		}
 
@@ -187,17 +274,10 @@ public class MainActivity extends Activity {
 
 	private void onEndTurn() {
 		mNumberOfTurns += 1;
-
 		mScore += mTurnScore;
 		mTurnScore = 0;
-
-		mTextTurnCounter.setText("Turns: " +mNumberOfTurns);
-		mTextScore.setText("Score: " +mScore);
-		mTextTurnScore.setText("Turn score: " +mTurnScore);
-
-		mButtonSave.setEnabled(false);
-		mButtonThrow.setEnabled(true);
-		
-		isNewTurn  = true;
+		mIsSavedEnabled = false;
+		mIsThrowEnabled = true;
+		mIsNewTurn  = true;
 	}
 }
